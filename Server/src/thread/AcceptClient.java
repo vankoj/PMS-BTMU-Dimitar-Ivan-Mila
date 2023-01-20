156b3a14bad165ccc8d92e1395f9f813d3ec3b56
vankoj <51315090+vankoj@users.noreply.github.com>
2023-01-20 02:15:49 +0200

    Reply with echo response if the command is not test message

--- Server/src/thread/AcceptClient.java
+++ Server/src/thread/AcceptClient.java
@@ -2,7 +2,9 @@ package thread;
 
 import model.command.Command;
 import model.command.SendMessageCommand;
+import model.enums.CommandType;
 import parsers.MessageParser;
+import tools.Generator;
 
 
 import java.io.*;
@@ -29,12 +31,7 @@ public class AcceptClient implements Runnable {
             System.out.println("\u001B[33mWaiting for client...\u001B[0m\n");
             socket = this.serverSocket.accept();
 
-            // call addClient() from Server class with reflection
-            Class<?> serverClass = Class.forName("Server");
-            Field field = serverClass.getField("clientsConnected");
-            field.setAccessible(true);
-            field.set(serverClass, (Integer) field.get(serverClass) + 1);
-            serverClass.getMethod("addClient").invoke(serverClass);
+            Class.forName("Server").getMethod("addClient").invoke(null); // TODO - don't use reflection
 
             System.out.println("\u001B[36mAccepted a client request.");
 
@@ -43,74 +40,36 @@ public class AcceptClient implements Runnable {
             System.out.println("Client address: " + clientAddress +
                     "\nClient port: " + clientPort + "\u001B[0m\n");
 
-            InputStream inputStream = socket.getInputStream();
-            InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
-            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+            ObjectInputStream inputStream = new ObjectInputStream(socket.getInputStream());
+            ObjectOutputStream outputStream = new ObjectOutputStream(socket.getOutputStream());
 
-            OutputStream outputStream = socket.getOutputStream();
-            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(outputStream);
-            BufferedWriter bufferedWriter = new BufferedWriter(outputStreamWriter);
-            PrintWriter printWriter = new PrintWriter(bufferedWriter);
+            String request = (String) inputStream.readObject();
+            Command command = MessageParser.parse(request);
 
-            String message = bufferedReader.readLine();
-            while (true) {
-                System.out.print(String.format("\u001B[35mclient %d: \u001B[0m", clientPort));
-                System.out.println(message);
-                if (message.equals("exit") || message.equals("quit")) break;
+            System.out.printf("\u001B[35mclient %d: \u001B[0m", clientPort);
+            System.out.println(command.serialize());
 
-                String reply = "";
-
-                Command command = MessageParser.parse(message);
-                switch (command.getType()) {
-                    case REGISTER:
-                        // TODO - implement
-                        break;
-                    case LOGIN:
-                        // TODO - implement
-                        break;
-                    case LOGOUT:
-                        // TODO - implement
-                        break;
-                    case SEND_MSG:
-                        if (command instanceof SendMessageCommand) {
-                            SendMessageCommand sendMessageCommand = (SendMessageCommand) command;
-                            reply = MessageParser.parseTestMessage(sendMessageCommand.getMessage());
-                            if (reply == null)
-                                reply = "";
-                        }
-                        break;
-                    case FRIEND_REQ:
-                        // TODO - implement
-                        break;
-                    case INVALID:
-                        // TODO - implement
-                        break;
-                }
-                if (reply.isEmpty()) {
-                    reply = String.format("Received %s command\n", command.getType().toString());
-                    reply += command;
-                }
-
-                System.out.print("me: " + reply + "\n");
-                printWriter.println(reply);
-                printWriter.flush();
-                if (reply.equals("exit") || reply.equals("quit")) {
-                    Thread.sleep(1000);
-                    break;
-                }
-
-                message = bufferedReader.readLine();
+            String response = "";
+            Command responseCommand = new Command(CommandType.INVALID, response);
+            if (command instanceof SendMessageCommand) {
+                SendMessageCommand sendMessageCommand = (SendMessageCommand) command;
+                response = MessageParser.parseTestMessage(sendMessageCommand.getMessage());
+                if (response != null)
+                    responseCommand = Generator.generateSendMessageCommand(((SendMessageCommand) command).getUsername(), ((SendMessageCommand) command).getChatName(), response);
+            } else {
+                responseCommand = command;
             }
 
-            bufferedReader.close();
-            printWriter.close();
+            System.out.print("me: " + responseCommand + "\n");
+            outputStream.writeObject(responseCommand.serialize());
+
+            inputStream.close();
+            outputStream.close();
         } catch
         (IOException | InvocationTargetException | NoSuchMethodException | ClassNotFoundException |
-         InterruptedException | IllegalAccessException
+         IllegalAccessException
                         ex) {
             ex.printStackTrace();
-        } catch (NoSuchFieldException e) {
-            throw new RuntimeException(e);
         } finally {
             try {
                 Field f = Class.forName("Server").getDeclaredField("clientsConnected");
@@ -119,10 +78,10 @@ public class AcceptClient implements Runnable {
                     f.set(null, (Integer) f.get(null) - 1);
 
                 String portMessage = (clientPort == null) ? "" : String.format(" with port %d", clientPort);
-                System.out.println(String.format("Closing client socket%s...", portMessage));
+                System.out.printf("Closing client socket%s...%n", portMessage);
                 assert socket != null;
                 socket.close();
-                System.out.println(String.format("Client socket%s closed.", portMessage));
+                System.out.printf("Client socket%s closed.%n", portMessage);
             } catch (IOException | NoSuchFieldException | ClassNotFoundException | IllegalAccessException ex) {
                 ex.printStackTrace();
             }
